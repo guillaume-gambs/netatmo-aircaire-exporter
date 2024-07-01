fe9dea8a88a867a58138edba51b741c47de57cdf
Guillaume Gambs <guillaume.gambs@digimind.com>
2024-07-01 15:31:28 +0200

    Add version management

--- version.py
+++ version.py
@@ -1,4 +1,3 @@
-# version.py
-__version__ = "unknown"
-__commit_sha__ = "unknown"
-__git_tag__ = "unknown"
+__version__ = "v-1.0.1-7-g26a3184"
+__commit_sha__ = "26a3184a1e4a494a5c061d706c2b8fc336731b48"
+__git_tag__ = "v-1.0.1-7-g26a3184"
