f874e058526b57749db1558fe888d1f778ee9fe5
Guillaume Gambs <guillaume.gambs@onclusive.com>
2026-05-12 14:48:12 +0200

    chore: bump version to v2.0.0

--- version.py
+++ version.py
@@ -1,3 +1,3 @@
-__version__ = "v-1.1.0-5-gef76596"
-__commit_sha__ = "ef7659628f376c5525517f098083b68b8b90bb54"
-__git_tag__ = "v-1.1.0-5-gef76596"
+__version__ = "v2.0.0"
+__commit_sha__ = "e208579ab6c1e06f554c230fead887e948e35b40"
+__git_tag__ = "v2.0.0"
